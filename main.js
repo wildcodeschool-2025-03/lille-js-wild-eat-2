83277db21c8cbdf3ed4f7699ab4de9a106657d6a
ChrisG-WCS <godet.chris59@gmail.com>
2025-03-27 17:10:24 +0100

    Carrousel fixe responsive

--- main.js
+++ main.js
@@ -6,11 +6,12 @@ const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
 
 const restaurants = [
-    { image: "image/Pepe.webp", description: "Pepe Chicken by FastGood Cuisine", details: "Le poulet rôti gourmand et savoureux qui réveille tes papilles à chaque bouchée.", },
-    { image: "image/Marta.jpg", description: "Marta Lille - Restaurant trendy de la vie lilloise", details: "Une cuisine raffinée et audacieuse, parfaite pour une pause gourmande." },
-    { image: "image/La-famille.jpg", description: "La Famille, Finest Lunch - Restaurants - Traiteur - Cantine", details: "Une cuisine conviviale et généreuse, comme à la maison, mais en mieux." },
-    { image: "image/LeBarboteur.jpg", description: "Le Barboteur–Restaurant & Bar, Quai de l'Ouest Lille", details: "Le resto au bord de l’eau qui va faire vibrer vos papilles !" },
+    { image: "image/Pepe.webp", description: "Pepe Chicken by FastGood Cuisine", details: "Le poulet rôti gourmand et savoureux qui réveille tes papilles à chaque bouchée.", link: "https://www.pepechicken.fr/" },
+    { image: "image/Marta.jpg", description: "Marta Lille - Restaurant trendy de la vie lilloise", details: "Une cuisine raffinée et audacieuse, parfaite pour une pause gourmande.", link: "https://example.com/marta" },
+    { image: "image/La-famille.jpg", description: "La Famille, Finest Lunch - Restaurants - Traiteur - Cantine", details: "Une cuisine conviviale et généreuse, comme à la maison, mais en mieux.", link: "https://example.com/famille" },
+    { image: "image/LeBarboteur.jpg", description: "Le Barboteur–Restaurant & Bar, Quai de l'Ouest Lille", details: "Le resto au bord de l’eau qui va faire vibrer vos papilles !", link: "https://example.com/barboteur" },
 ];
+
 
 let currentIndex = 0;
 let isTransitioning = false;
